8e990a188d6fdd74cb6894ebd0a98432eaf56531
ktchanha <anokuchukhidze@gmail.com>
2026-08-27 18:27:36 +0400

    complete JavaScript assignments

---
+++ exercise_9.js
@@ -0,0 +1,9 @@
+console.log(Boolean(""));
+console.log(Boolean("0"));
+console.log(Boolean(" "));
+console.log(Boolean(0));
+console.log(Boolean(-5));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(true));
+console.log(Boolean(false));
